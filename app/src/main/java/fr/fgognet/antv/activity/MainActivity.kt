26fd89f800abaeaa64619da999e3ff5efc8a489f
francois76 <fgognet@gmail.com>
2022-07-20 21:11:16 +0200

    update

--- app/src/main/java/fr/fgognet/antv/activity/MainActivity.kt
+++ app/src/main/java/fr/fgognet/antv/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.fgognet.antv.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.NavHostFragment
 import fr.fgognet.antv.R
@@ -18,6 +19,13 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LOW_PROFILE)
         setContentView(R.layout.activity_main)
 
     }
