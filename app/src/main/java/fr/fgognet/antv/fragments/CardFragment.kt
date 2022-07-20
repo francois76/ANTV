31ddef8ead04005f1cd41547001881ecfd3c08ba
francois76 <fgognet@gmail.com>
2022-07-20 22:35:18 +0200

    update

--- app/src/main/java/fr/fgognet/antv/fragments/CardFragment.kt
+++ app/src/main/java/fr/fgognet/antv/fragments/CardFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -81,7 +82,7 @@ class CardFragment : Fragment() {
                 }
             }
         }
-        view.setOnClickListener {
+        view.findViewById<Button>(R.id.live_button).setOnClickListener {
             val bundle = Bundle()
             bundle.putString("url", live)
             Navigation.findNavController(it).navigate(R.id.playerFragment, bundle)
