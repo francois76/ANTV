5a41abd9986db10388503ce663296a39d285e328
francois76 <fgognet@gmail.com>
2022-07-23 22:25:44 +0200

    update

--- app/src/main/java/fr/fgognet/antv/activity/MainActivity.kt
+++ app/src/main/java/fr/fgognet/antv/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package fr.fgognet.antv.activity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.View
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.cast.framework.CastButtonFactory
@@ -22,12 +21,6 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.v(TAG, "onCreate")
         super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LOW_PROFILE)
         setContentView(R.layout.activity_main)
         CastButtonFactory.setUpMediaRouteButton(
             applicationContext,
