b6a44951d276466af4cd3f45552008f66362b76f
francois76 <fgognet@gmail.com>
2022-07-24 23:29:37 +0200

    update

---
+++ app/src/main/java/fr/fgognet/antv/bean/EventSearch.kt
@@ -0,0 +1,18 @@
+package fr.fgognet.antv.bean
+
+data class EventSearch(
+
+    var nid: Int?,
+    var mediaId: Int?,
+    var date: String?,
+    var thumbnail: String?,
+    var published: Boolean,
+    var vis: String?,
+    var description: String?,
+    var video_type: String?,
+    var theme: String?,
+    var commission: String?,
+    var title: String?,
+    var url: String?,
+    var size: Int?
+)
