6e695a753785066696bff58fe401fbc95791dc24
francois76 <fgognet@gmail.com>
2022-08-09 20:55:21 +0200

    refactoring

--- app/src/main/java/fr/fgognet/antv/view/cardList/playlist/PlaylistFragment.kt
+++ app/src/main/java/fr/fgognet/antv/view/cardList/playlist/PlaylistFragment.kt
@@ -14,11 +14,6 @@ import fr.fgognet.antv.external.image.ImageRepository
 import fr.fgognet.antv.view.cardList.AbstractCardListFragment
 import fr.fgognet.antv.view.cardList.AbstractCardListViewModel
 import fr.fgognet.antv.view.cardList.CardData
-import fr.fgognet.antv.view.cardList.CardType
-import fr.fgognet.antv.view.player.ARG_DESCRIPTION
-import fr.fgognet.antv.view.player.ARG_IMAGE_CODE
-import fr.fgognet.antv.view.player.ARG_TITLE
-import fr.fgognet.antv.view.player.ARG_URL
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -61,36 +56,20 @@ class PlaylistFragment : AbstractCardListFragment() {
         cardImageView.contentDescription = cardData.title
         buttonView.isEnabled = cardData.clickable
         buttonView.text = cardData.buttonLabel
-        if (cardData.buttonBackgroundColorId != 0) {
-            val background = TypedValue()
-            context?.theme?.resolveAttribute(
-                cardData.buttonBackgroundColorId,
-                background,
-                true
-            )
-            buttonView.setBackgroundColor(
-                background.data
-            )
-            buttonView.setTextColor(Color.WHITE)
-        }
+        val background = TypedValue()
+        context?.theme?.resolveAttribute(
+            cardData.buttonBackgroundColorId,
+            background,
+            true
+        )
+        buttonView.setBackgroundColor(
+            background.data
+        )
+        buttonView.setTextColor(Color.WHITE)
         buttonView.setOnClickListener {
-            if (cardData.cardType == CardType.PLAYLIST) {
-                Navigation.findNavController(it)
-                    .navigate(R.id.replayFragment, cardData.targetBundle)
-            } else {
-                val bundle = Bundle()
-                bundle.putString(ARG_URL, cardData.url)
-                bundle.putString(ARG_TITLE, cardData.title)
-                bundle.putString(
-                    ARG_DESCRIPTION,
-                    cardData.description
-                )
-                bundle.putString(
-                    ARG_IMAGE_CODE,
-                    cardData.imageCode
-                )
-                Navigation.findNavController(it).navigate(R.id.playerFragment, bundle)
-            }
+            Navigation.findNavController(it)
+                .navigate(R.id.replayFragment, cardData.targetBundle)
+
         }
         scope.launch {
             withContext(Dispatchers.IO) {
