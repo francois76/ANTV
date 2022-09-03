88184c824cbdce305c99dc88f8765717e9d758a3
francoisgognet <fgognet@gmail.com>
2022-09-03 20:56:33 +0200

    updating routing

--- shared/src/commonMain/kotlin/fr/fgognet/antv/view/cardList/playlist/PlaylistViewModel.kt
+++ shared/src/commonMain/kotlin/fr/fgognet/antv/view/cardList/playlist/PlaylistViewModel.kt
@@ -1,7 +1,6 @@
 package fr.fgognet.antv.view.cardList.playlist
 
 import fr.fgognet.antv.external.eventSearch.EventSearchQueryParams
-import fr.fgognet.antv.mapping.Bundle
 import fr.fgognet.antv.view.cardList.AbstractCardListViewModel
 import fr.fgognet.antv.view.cardList.CardListViewData
 import io.github.aakira.napier.Napier
@@ -20,12 +19,9 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Dernières publications",
                         "Les dernières publications",
                         "https://videos.assemblee-nationale.fr/Datas/an/12053682_62cebe5145c82/files/S%C3%A9ance.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Dernières publications"
-                            )
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Dernières publications"
                             return b
                         }(),
                     ),
@@ -34,16 +30,10 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Questions au gouvernement",
                         "Toute les questions au gouvernement",
                         "https://videos.assemblee-nationale.fr/Datas/an/12053682_62cebe5145c82/files/S%C3%A9ance.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Questions au gouvernement"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Questions au gouvernement"
-                            )
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Questions au gouvernement"
+                            b[EventSearchQueryParams.TypeVideo] = "Questions au gouvernement"
                             return b
                         }(),
                     ),
@@ -52,16 +42,10 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Séance publique",
                         "Toutes les séances publiques ainsi que les questions au gouvernement non montées",
                         "https://videos.assemblee-nationale.fr/live/images/14000000.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Séance publique"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Séance publique"
-                            )
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Séance publique"
+                            b[EventSearchQueryParams.TypeVideo] = "Séance publique"
                             return b
                         }(),
                     ),
@@ -70,20 +54,12 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Commission au développement durable",
                         "Tous les replays de la commission au développement durable",
                         "https://videos.assemblee-nationale.fr/live/images/419865.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Commission"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Commission.toString(),
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Commission du développement durable"
+                            b[EventSearchQueryParams.TypeVideo] = "Commission"
+                            b[EventSearchQueryParams.Commission] =
                                 "Développement durable (commission)"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Commission du développement durable"
-                            )
                             return b
                         }(),
                     ),
@@ -92,20 +68,12 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Commission des affaires économiques",
                         "Tous les replays de la commission des affaires économiques",
                         "https://videos.assemblee-nationale.fr/live/images/419610.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Commission"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Commission.toString(),
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Commission des affaires économiques"
+                            b[EventSearchQueryParams.TypeVideo] = "Commission"
+                            b[EventSearchQueryParams.Commission] =
                                 "Affaires économiques (commission)"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Commission des affaires économiques"
-                            )
                             return b
                         }(),
                     ),
@@ -114,20 +82,13 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Commission des affaires culturelles et éducation",
                         "Tous les replays de la commission des affaires culturelles et éducation",
                         "https://videos.assemblee-nationale.fr/live/images/419604.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Commission"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Commission.toString(),
-                                "Affaires culturelles et éducation (commission)"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] =
                                 "Commission des affaires culturelles et éducation"
-                            )
+                            b[EventSearchQueryParams.TypeVideo] = "Commission"
+                            b[EventSearchQueryParams.Commission] =
+                                "Affaires culturelles et éducation (commission)"
                             return b
                         }(),
                     ),
@@ -136,20 +97,12 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Commission des affaires européennes",
                         "Tous les replays de la commission des affaires européennes",
                         "https://videos.assemblee-nationale.fr/live/images/415287.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Commission"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Commission.toString(),
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Commission des affaires européennes"
+                            b[EventSearchQueryParams.TypeVideo] = "Commission"
+                            b[EventSearchQueryParams.Commission] =
                                 "Affaires européennes (commission)"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Commission des affaires européennes"
-                            )
                             return b
                         }(),
                     ),
@@ -158,20 +111,12 @@ class NewPlaylistViewModel : AbstractCardListViewModel<PlaylistCardData>() {
                         "Commission des affaires étrangères",
                         "Tous les replays de la commission des affaires étrangères",
                         "https://videos.assemblee-nationale.fr/live/images/59047.jpg",
-                        fun(): Bundle {
-                            val b = Bundle()
-                            b.putString(
-                                EventSearchQueryParams.TypeVideo.toString(),
-                                "Commission"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Commission.toString(),
+                        fun(): Map<EventSearchQueryParams, String> {
+                            val b = hashMapOf<EventSearchQueryParams, String>()
+                            b[EventSearchQueryParams.Tag] = "Commission des affaires étrangères"
+                            b[EventSearchQueryParams.TypeVideo] = "Commission"
+                            b[EventSearchQueryParams.Commission] =
                                 "Affaires étrangères (commission)"
-                            )
-                            b.putString(
-                                EventSearchQueryParams.Tag.toString(),
-                                "Commission des affaires étrangères"
-                            )
                             return b
                         }(),
                     )
