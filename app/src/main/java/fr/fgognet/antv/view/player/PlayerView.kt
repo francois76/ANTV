13cccfd8a9abb0f0244838e4a3ba7efef0c614e7
francoisgognet <fgognet@gmail.com>
2022-09-04 14:57:39 +0200

    end cleaning legacy code

--- app/src/main/java/fr/fgognet/antv/view/player/PlayerView.kt
+++ app/src/main/java/fr/fgognet/antv/view/player/PlayerView.kt
@@ -3,11 +3,12 @@ package fr.fgognet.antv.view.player
 import android.content.Context
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.exoplayer2.Player
@@ -17,6 +18,7 @@ import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.soywiz.korim.format.toAndroidBitmap
 import dev.icerock.moko.mvvm.createViewModelFactory
 import fr.fgognet.antv.R
+import fr.fgognet.antv.databinding.FragmentPlayerBinding
 import fr.fgognet.antv.external.image.ImageRepository
 import fr.fgognet.antv.service.player.MediaData
 import fr.fgognet.antv.service.player.PlayerService
@@ -39,56 +41,53 @@ fun PlayerView(
                 ?: ""]?.toAndroidBitmap()
         )
     )
-    PlayerView(setVisible = setVisible)
+    PlayerView(description = description, setVisible = setVisible)
 }
 
 @Composable
-fun PlayerView(setVisible: (visible: Boolean) -> Unit) {
+fun PlayerView(description: String?, setVisible: (visible: Boolean) -> Unit) {
     val context: Context = LocalContext.current.applicationContext
     val model: PlayerViewModel = viewModel(factory = createViewModelFactory {
         PlayerViewModel().start(context)
     }
     )
     val state by model.player.ld().observeAsState()
-    PlayerViewState(player = state, setVisible = setVisible)
+    PlayerViewState(description = description, player = state, setVisible = setVisible)
 
 }
 
 @Composable
-fun PlayerViewState(player: Player?, setVisible: (visible: Boolean) -> Unit) {
-    LocalContext.current.findActivity()?.window?.decorView?.keepScreenOn = true
-    AndroidView(
-        factory = { context ->
-            // view.rootView.findViewById<MaterialToolbar>(R.id.topAppBar).title =       it.mediaMetadata.title
-            // view.findViewById<TextView>(R.id.video_description)?.text = mediaData?.description
-            val view = StyledPlayerView(context)
-            if (player is CastPlayer) {
-                view.controllerHideOnTouch = false
-                view.controllerShowTimeoutMs = 0
-                view.showController()
-                view.defaultArtwork = ResourcesCompat.getDrawable(
-                    context.resources!!,
-                    R.drawable.ic_baseline_cast_connected_400,
-                    context.theme
-                )
-            } else { // currentPlayer == localPlayer
-                view.controllerHideOnTouch = false
-                view.controllerShowTimeoutMs = StyledPlayerControlView.DEFAULT_SHOW_TIMEOUT_MS
-                view.defaultArtwork = null
-            }
-            view.player = player
-            view.setControllerVisibilityListener(StyledPlayerView.ControllerVisibilityListener { visibility: Int ->
-                Log.v(TAG, "Player controler visibility Changed: $visibility")
-                when (visibility) {
-                    View.VISIBLE -> {
-                        setVisible(true)
-                    }
-                    View.GONE -> {
-                        setVisible(false)
-                    }
-                }
-            })
-            view
+fun PlayerViewState(description: String?, player: Player?, setVisible: (visible: Boolean) -> Unit) {
+    val context = LocalContext.current
+    context.findActivity()?.window?.decorView?.keepScreenOn = true
+    AndroidViewBinding(factory = FragmentPlayerBinding::inflate) {
+        // view.rootView.findViewById<MaterialToolbar>(R.id.topAppBar).title =       it.mediaMetadata.title
+        videoView.findViewById<TextView>(R.id.video_description)?.text = description
+        if (player is CastPlayer) {
+            videoView.controllerHideOnTouch = false
+            videoView.controllerShowTimeoutMs = 0
+            videoView.showController()
+            videoView.defaultArtwork = ResourcesCompat.getDrawable(
+                context.resources!!,
+                R.drawable.ic_baseline_cast_connected_400,
+                context.theme
+            )
+        } else { // currentPlayer == localPlayer
+            videoView.controllerHideOnTouch = false
+            videoView.controllerShowTimeoutMs = StyledPlayerControlView.DEFAULT_SHOW_TIMEOUT_MS
+            videoView.defaultArtwork = null
         }
-    )
+        videoView.player = player
+        videoView.setControllerVisibilityListener(StyledPlayerView.ControllerVisibilityListener { visibility: Int ->
+            Log.v(TAG, "Player controler visibility Changed: $visibility")
+            when (visibility) {
+                View.VISIBLE -> {
+                    setVisible(true)
+                }
+                View.GONE -> {
+                    setVisible(false)
+                }
+            }
+        })
+    }
 }
