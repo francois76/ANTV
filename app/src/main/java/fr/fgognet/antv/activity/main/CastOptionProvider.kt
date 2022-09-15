4ac96f9086d4837eb21bc7af53618b019a1b40b7
francoisgognet <fgognet@gmail.com>
2022-09-15 21:40:59 +0200

    rollback cast change

---
+++ app/src/main/java/fr/fgognet/antv/activity/main/CastOptionProvider.kt
@@ -0,0 +1,26 @@
+package fr.fgognet.antv.activity.main
+
+import android.content.Context
+import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+
+// CastOptionsProvider provided in manifest, do not remove
+class CastOptionsProvider : OptionsProvider {
+
+    override fun getCastOptions(context: Context): CastOptions {
+        return CastOptions.Builder()
+            .setResumeSavedSession(false)
+            .setEnableReconnectionService(false)
+            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .setStopReceiverApplicationWhenEndingSession(true)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider>? {
+        return emptyList()
+    }
+
+
+}
