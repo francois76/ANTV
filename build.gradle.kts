2f9e9b12ed7d78b7f8f153e0748572f9177ab161
francoisgognet <fgognet@gmail.com>
2023-04-27 19:39:18 +0200

    init directory refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once https://github.com/gradle/gradle/issues/22797 is fixed (should be gradle 8.1)
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
