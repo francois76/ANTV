b6cfe78008a9912376385b7abe15d2511ba491ec
francoisgognet <fgognet@gmail.com>
2023-04-28 15:20:06 +0200

    init directory refactoring

--- shared/src/desktopMain/kotlin/fr/fgognet/antv/widget/HtmlText.kt
+++ shared/src/desktopMain/kotlin/fr/fgognet/antv/widget/HtmlText.kt
@@ -1,8 +1,10 @@
 package fr.fgognet.antv.widget
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-expect fun HtmlText(html: String, modifier: Modifier = Modifier)
-
+actual fun HtmlText(html: String, modifier: Modifier) {
+    Text(html)
+}
