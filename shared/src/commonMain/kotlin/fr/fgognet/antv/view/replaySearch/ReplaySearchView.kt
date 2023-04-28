d26c63f06691e22bdcf7196944c40b4f005d1a04
francoisgognet <fgognet@gmail.com>
2023-04-28 15:33:53 +0200

    init directory refactoring

--- shared/src/commonMain/kotlin/fr/fgognet/antv/view/replaySearch/ReplaySearchView.kt
+++ shared/src/commonMain/kotlin/fr/fgognet/antv/view/replaySearch/ReplaySearchView.kt
@@ -1,23 +1,17 @@
 package fr.fgognet.antv.view.replaySearch
 
-import android.icu.util.Calendar
-import android.widget.CalendarView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.viewinterop.AndroidView
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import dev.icerock.moko.resources.compose.stringResource
 import fr.fgognet.antv.MR
-import fr.fgognet.antv.widget.buildColors
 
 
 private var currentDate: Long = 0L
@@ -29,38 +23,27 @@ fun ReplaySearchView(
     }, key = "ReplaySearchViewModel"),
     query: (queryParams: Unit) -> Unit
 ) {
-    MaterialTheme(colorScheme = buildColors(context = LocalContext.current)) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = Dp(20F))
-        ) {
-            lateinit var calendarView: CalendarView
-            AndroidView(
-                {
-                    calendarView = CalendarView(it)
-                    calendarView
-                },
-                modifier = Modifier
-                    .fillMaxWidth(),
-                update = { views ->
-                    views.setOnDateChangeListener { _, year, month, day ->
-                        val c: Calendar = Calendar.getInstance()
-                        c.set(year, month, day)
-                        currentDate = c.timeInMillis
-                    }
-                }
-            )
-            Button(
-                onClick = {
-                    query(model.makeSearchBundle(currentDate))
-                },
-                content = {
-                    Text(text = stringResource(resource = MR.strings.buttom_search))
-                },
-                modifier = Modifier.fillMaxWidth(),
-            )
-        }
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Dp(20F))
+    ) {
+        DatePicker(currentDate = currentDate, onCurrentDateChange = { date ->
+            currentDate = date
+        })
+        Button(
+            onClick = {
+                query(model.makeSearchBundle(currentDate))
+            },
+            content = {
+                Text(text = stringResource(resource = MR.strings.buttom_search))
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 
 }
+
+@Composable
+expect fun DatePicker(currentDate: Long, onCurrentDateChange: (date: Long) -> Unit)
+
