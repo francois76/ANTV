aca698417858833c2be0283f47b9a0cf3dffba60
francoisgognet <fgognet@gmail.com>
2023-04-28 13:02:59 +0200

    init directory refactoring

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             isDebuggable = true
         }
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes.addAll(
                 arrayListOf(
