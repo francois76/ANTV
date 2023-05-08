394577b5f76ceaf4a119930e3e68632602b6b7cd
francoisgognet <fgognet@gmail.com>
2023-05-08 22:59:30 +0200

    init directory refactoring

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,22 +2,9 @@ import UIKit
 import SwiftUI
 import shared
 
-let gradient = LinearGradient(
-        colors: [
-            Color.black.opacity(0.6),
-            Color.black.opacity(0.6),
-            Color.black.opacity(0.5),
-            Color.black.opacity(0.3),
-            Color.black.opacity(0.0),
-        ],
-        startPoint: .top, endPoint: .bottom
-)
-
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        let controller = Main_iosKt.MainViewController()
-        controller.overrideUserInterfaceStyle = .light
-        return controller
+        Main_iosKt.MainViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
@@ -25,13 +12,11 @@ struct ComposeView: UIViewControllerRepresentable {
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            ComposeView()
-                    .ignoresSafeArea(.all) // Compose has own keyboard handler
-            VStack {
-                gradient.ignoresSafeArea(edges: .top).frame(height: 0)
-                Spacer()
-            }
-        }.preferredColorScheme(.dark)
+        ComposeView()
+                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
+
+
+
+
---
+++ shared/src/iosMain/kotlin/fr/fgognet/antv/main.ios.kt
@@ -0,0 +1,14 @@
+package fr.fgognet.antv
+
+/*
+ * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+import androidx.compose.ui.window.ComposeUIViewController
+import fr.fgognet.antv.view.main.ANTVApp
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController {
+    ANTVApp()
+}
