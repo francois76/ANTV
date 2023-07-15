8f9ad2fd941bae44ce71d1a4c74d672442db887b
francoisgognet <fgognet@gmail.com>
2023-07-15 19:25:11 +0200

    cleaning

--- shared/src/androidMain/kotlin/fr/fgognet/antv/widget/modal.android.kt
+++ shared/src/androidMain/kotlin/fr/fgognet/antv/widget/modal.android.kt
@@ -4,7 +4,6 @@ import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 
@@ -40,9 +39,8 @@ actual fun Modal(
 }
 
 @Composable
-fun HtmlText(html: String, modifier: Modifier) {
+fun HtmlText(html: String) {
     AndroidView(
-        modifier = modifier,
         factory = { context ->
             val text = TextView(context)
             text.movementMethod = LinkMovementMethod.getInstance()
