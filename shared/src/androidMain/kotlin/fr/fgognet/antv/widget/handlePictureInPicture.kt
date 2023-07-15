f36c149bdf3f59c2e5721c6d55296b476ee4e68e
francoisgognet <fgognet@gmail.com>
2023-07-15 23:13:18 +0200

    fixed deepLink

--- shared/src/androidMain/kotlin/fr/fgognet/antv/widget/handlePictureInPicture.kt
+++ shared/src/androidMain/kotlin/fr/fgognet/antv/widget/handlePictureInPicture.kt
@@ -5,12 +5,12 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import com.chrynan.navigation.ExperimentalNavigationApi
-import com.chrynan.navigation.Navigator
-import com.chrynan.navigation.SingleNavigationContext
-import com.chrynan.navigation.goTo
+import com.chrynan.navigation.*
 import fr.fgognet.antv.view.main.Route
 import fr.fgognet.antv.view.main.RouteData
+import io.github.aakira.napier.Napier
+
+private const val TAG = "ANTV/HandlePictureInPictureWidget"
 
 @OptIn(ExperimentalNavigationApi::class)
 @Composable
@@ -21,7 +21,7 @@ actual fun HandlePictureInPicture(
     val lifecycleOwner = LocalLifecycleOwner.current
     DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
-
+            Napier.v(tag = TAG, message = "event received : $event")
             when (event) {
                 Lifecycle.Event.ON_PAUSE -> {
                     if (context.findActivity()?.isInPictureInPictureMode == true) {
