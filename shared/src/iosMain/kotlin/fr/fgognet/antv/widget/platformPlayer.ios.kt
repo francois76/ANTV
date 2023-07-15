308b0680768d896def808da15e9e3066798c9a54
francoisgognet <fgognet@gmail.com>
2023-07-15 19:57:10 +0200

    sample player

--- shared/src/iosMain/kotlin/fr/fgognet/antv/widget/platformPlayer.ios.kt
+++ shared/src/iosMain/kotlin/fr/fgognet/antv/widget/platformPlayer.ios.kt
@@ -14,26 +14,29 @@ import platform.UIKit.UIView
 
 @Composable
 actual fun Player(modifier: Modifier, context: PlatformContext, controller: MediaController) {
+
+    // A streamController object is defined to handle the AVPlayerViewController event.
+    var streamController = AVPlayerViewController()
+
+    // Stream Video URL value video related information - url info
+    val streamVideoURL: NSURL =
+        NSURL(string = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4")
+
+    val player = AVPlayer(uRL = streamVideoURL)
+    val playerLayer = AVPlayerLayer()
+    playerLayer.player = player;
+    streamController.player = player;
+
+    streamController.player?.play()
     UIKitView(
         modifier = modifier,
         factory = {
-            val view = UIView()
-
-            // A streamController object is defined to handle the AVPlayerViewController event.
-            var streamController = AVPlayerViewController()
-
-            // Stream Video URL value video related information - url info
-            val streamVideoURL: NSURL =
-                NSURL(string = "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4")
-
-            val player = AVPlayer(uRL = streamVideoURL)
-            val playerLayer = AVPlayerLayer()
-            playerLayer.player = player;
-            streamController.player = player;
-
-            view.layer.addSublayer(playerLayer)
-            streamController.player?.play()
-            view
+            UIView().apply {
+                layer.addSublayer(playerLayer)
+            }
+        }, onResize = { view, rect ->
+            view.setFrame(rect)
+            playerLayer.setFrame(rect)
         }
     )
 }
