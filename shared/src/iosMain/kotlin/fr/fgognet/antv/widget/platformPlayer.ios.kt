8fd57490b15d7c98884e104ed725375b506162e5
francoisgognet <fgognet@gmail.com>
2023-07-16 22:38:45 +0200

    init ios player codebase

--- shared/src/iosMain/kotlin/fr/fgognet/antv/widget/platformPlayer.ios.kt
+++ shared/src/iosMain/kotlin/fr/fgognet/antv/widget/platformPlayer.ios.kt
@@ -3,16 +3,18 @@ package fr.fgognet.antv.widget
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.interop.UIKitView
 import io.github.aakira.napier.Napier
 import platform.AVFoundation.AVPlayerLayer
+import platform.AVFoundation.currentItem
 import platform.UIKit.UIView
 
 private const val TAG = "ANTV/WidgetPlayer"
 
 @Composable
 actual fun Player(modifier: Modifier, context: PlatformContext, controller: MediaController) {
-    if (controller.iosMediaController?.player == null) {
+    if (controller.iosMediaController?.player == null || controller.iosMediaController.player?.currentItem == null) {
         return
     }
     val playerLayer = remember {
@@ -28,6 +30,7 @@ fun ViewKit(modifier: Modifier, playerLayer: AVPlayerLayer) {
     Napier.v(tag = TAG, message = "ViewKit")
     UIKitView(
         modifier = modifier,
+        background = Color.Black,
         factory = {
             Napier.v(tag = TAG, message = "building UIKIT Player")
             UIView().apply {
