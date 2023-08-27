c9a7af77339802b278424eceb4d9a0f1049d1fc5
francoisgognet <fgognet@gmail.com>
2023-08-27 22:53:24 +0200

    fixed player not working

--- shared/src/commonMain/kotlin/fr/fgognet/antv/widget/toHour.kt
+++ shared/src/commonMain/kotlin/fr/fgognet/antv/widget/toHour.kt
@@ -6,7 +6,7 @@ fun Long.toHour(): String {
     return if (this <= 0L) {
         "..."
     } else {
-        val current = LocalTime.fromSecondOfDay(this.toInt())
+        val current = LocalTime.fromMillisecondOfDay(this.toInt())
         return "${current.hour}".padStart(2, '0') + ":" + "${current.minute}".padStart(
             2,
             '0'
